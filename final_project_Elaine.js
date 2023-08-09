0499d20af200b6164d061f2c08729c9d03e85ac3
Elaine-in-AI <141021324+Elaine-in-AI@users.noreply.github.com>
2023-08-09 19:56:15 +1000

    Add files via upload

--- final_project_Elaine.js
+++ final_project_Elaine.js
@@ -1,5 +1,5 @@
 
-    // Function to validate the form
+// Function to validate the form
     function validateForm(event) {
         event.preventDefault(); 
 
@@ -39,8 +39,62 @@
         }
     }
 
+        const forms = document.querySelectorAll('form')
+        forms.forEach(form => {
+        form.addEventListener('submit', validateForm);
+         });
 
-    const forms = document.querySelectorAll('form')
-  forms.forEach(form => {
-      form.addEventListener('submit', validateForm);
-    });
+// // This is the js for the API call which would contain back-end issues.
+// // openai API configuration
+//     import { Configuration, OpenAIApi } from "openai";
+//     const configuration = new Configuration({
+//         organization: organizationId,
+//         apiKey: apiKey,
+//     });
+//     const openai = new OpenAIApi(configuration);
+//     const engineResponse = await openai.listEngines();
+
+//     const apiKey = "";
+//     const organizationId = "";
+ 
+// // define function
+//     async function getRecipe(event) {
+//         event.preventDefault();
+    
+//         const inputFood = document.querySelector("#inputFood").value;
+    
+//     // Define apiUrl and requestData based on use case
+//         const apiUrl = "https://api.openai.com/v1/models";
+//         const requestData = {
+//             prompt: 'Suggest a recipe using the following ingredients: ${inputFood}',
+//             max_tokens: 100           
+//         };
+ 
+//         const response = await fetch(apiUrl, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': 'Bearer ${apiKey}',
+//                 'OpenAI-Organization': organizationId,
+//             },
+//             body: JSON.stringify(requestData)
+//         });
+    
+//         console.log(response);
+
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+    
+//         const data = await response.json();
+//         const recipe = data.choices[0].text;
+
+//         console.log(recipe);
+
+//         document.querySelector('#recipeResult').textContent = recipe;
+//     }
+    
+//     // Attach the getRecipe function to an event
+//     const getRecipeButton = document.querySelector('#getRecipeBtn');
+//     getRecipeButton.addEventListener('click', getRecipe);
+    
